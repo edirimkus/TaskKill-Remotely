98b655b59ff56c7c94fe59387908e8397d23d5ff
edirimkus <93425953+edirimkus@users.noreply.github.com>
2024-10-13 22:20:45 +0300

    Update remote-task-killer.bat

--- remote-task-killer.bat
+++ remote-task-killer.bat
@@ -1,16 +1,29 @@
 @echo off
+REM Initialize the process
+
 :OTHERPC
 set /P "CompName=Please enter the computer name: "
+
 :ANOTHERTASK
-set /P "TaskName=Please enter the task name: "
+set /P "TaskName=Please enter the task name or process ID: "
+
+REM Taskkill command to end the specified task on the remote computer
 taskkill /s %CompName% /pid %TaskName%* /f
+
+REM Pause to allow user to see the output
 pause
-@echo off
-ECHO y=Yes , n=No , o=Other PC 
-SET /P M=Kill Another Task ?:
-IF %M%==y GOTO ANOTHERTASK
-IF %M%==n GOTO NO
-IF %M%==o GOTO OTHERPC
+
+:AGAIN
+echo y=Yes , n=No , o=Other PC 
+set /P "M=Kill Another Task ?: "
+
+if /I "%M%"=="y" goto ANOTHERTASK
+if /I "%M%"=="n" goto NO
+if /I "%M%"=="o" goto OTHERPC
+
+:INVALID
+echo Invalid option. Please enter y, n, or o.
+goto AGAIN
 
 :NO
-EXIT
+exit
